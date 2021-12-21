79ead62686320c0fc924d6b5338b26b309aceae7
acharl <amadeo.charle108@gmail.com>
2021-12-21 10:40:39 +0100

    chore(): prepare release

--- packages/sapling-wasm/examples/test.ts
+++ packages/sapling-wasm/examples/test.ts
@@ -1,6 +1,6 @@
 
 import * as provotumAirGap from '../dist'
-// import BN = require('bn.js')
+import BN = require('bn.js')
 import crypto = require('crypto')
 
 const run = async() => {
@@ -11,13 +11,12 @@ try {
     const sealer = 'charlie'
     const [q, params, sk, pk] = await provotumAirGap.setupElgamal(secretKey)
 
-    // const rawByteSize = Buffer.byteLength(q.toString(), 'utf8')
-    // const byteSize = new BN(rawByteSize, 10)
-    // const targetValue: BN = new BN(q, 16)
+    const rawByteSize = Buffer.byteLength(q.toString(), 'utf8')
+    const byteSize = new BN(rawByteSize, 10)
+    const targetValue: BN = new BN(q, 16)
 
-    const r = '1'// getSecureRandomValue(targetValue, byteSize)
+    const r = getSecureRandomValue(targetValue, byteSize)
 
-     //call provotumAirGap.decrypt
 
      const encryptions = [
         {
@@ -10084,19 +10083,19 @@ run()
 
 
 
-//     // get a secure random value x: 0 < x < n
-// export const getSecureRandomValue = (n: BN, BYTE_SIZE: BN): BN => {
-//   let byteSize: number
-//   try {
-//     byteSize = BYTE_SIZE.toNumber()
-//   } catch {
-//     // https://www.ecma-international.org/ecma-262/5.1/#sec-8.5
-//     // used for large numbers from EC
-//     byteSize = 32
-//   }
+    // get a secure random value x: 0 < x < n
+export const getSecureRandomValue = (n: BN, BYTE_SIZE: BN): BN => {
+  let byteSize: number
+  try {
+    byteSize = BYTE_SIZE.toNumber()
+  } catch {
+    // https://www.ecma-international.org/ecma-262/5.1/#sec-8.5
+    // used for large numbers from EC
+    byteSize = 32
+  }
 
-//   let randomBytes: Buffer = crypto.randomBytes(byteSize)
-//   let randomValue: BN = new BN(randomBytes)
+  let randomBytes: Buffer = crypto.randomBytes(byteSize)
+  let randomValue: BN = new BN(randomBytes)
 
-//   return randomValue.mod(n)
-// }
+  return randomValue.mod(n)
+}
